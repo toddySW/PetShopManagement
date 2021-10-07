1d4305b4169f06b461be54b296723d2adfb36b90
toddy <tolbduy0044@gmail.com>
2021-10-07 19:29:08 +0700

    add all

---
+++ src/petmanagementapp/PetManagementApp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petmanagementapp;
+
+import com.yolo.gui.Home_GUI;
+
+/**
+ *
+ * @author User
+ */
+public class PetManagementApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home_GUI home = new Home_GUI();
+        home.setVisible(true);
+    }
+    
+}
